29b59e7e614c54e238dc727adb77aae91a9b3a89
AVViktoria <56619435+AVViktoria@users.noreply.github.com>
2022-11-12 14:35:00 -0800

    Update App.js

--- src/App/App.js
+++ src/App/App.js
@@ -14,55 +14,46 @@ import css from '../App/App.module.css';
 export default function App() {
   const [query, setQuery] = useState('');
   const [page, setPage] = useState(1);
-  const [imagesOnPage] = useState(0);
-  const [totalImages] = useState(0);
+  const [imagesOnPage, setImagesOnPage] = useState(0);
+  const [totalImages, setTotalImages] = useState(0);
   const [isLoading, setIsLoading] = useState(false);
   const [showModal, setShowModal] = useState(false);
-  const [images] = useState(null);
-  const [setError] = useState(null);
-  // const [currentImageUrl, setCurrentImageUrl] = useState(null);
-  // const [currentImageDescription, setCurrentImageDescription] = useState(null);
+  const [images, setImages] = useState(null);
+  // const [error, setError] = useState(null);
+  const [currentImageUrl, setCurrentImageUrl] = useState(null);
+  const [currentImageDescription, setCurrentImageDescription] = useState(null);
 
   //!     need to do it     //
   useEffect(() => {
+    if (query === '') {
+      return;
+    }
     setIsLoading(state => !state);
     fetchImages(query, page)
-      .then(({ hits, totalHits }) => {
-        const imagesArray = hits.map(hit => ({
-          id: hit.id,
-          description: hit.tags,
-          smallImage: hit.webformatURL,
-          largeImage: hit.largeImageURL,
-        }));
-
-        return this.setState({
-          page: 1,
-          images: imagesArray,
-          imagesOnPage: imagesArray.length,
-          totalImages: totalHits,
+      .then(res => {
+        const imagesArray = res.hits.map(hit => {
+          return {
+            id: hit.id,
+            description: hit.tags,
+            smallImage: hit.webformatURL,
+            largeImage: hit.largeImageURL,
+          };
         });
+        setImages(prev => [...prev, ...imagesArray]);
+        setImagesOnPage(prev => prev + imagesArray.length);
+        setTotalImages(res.totalImages);
       })
-
-      //!    переделать              //
-      // return setState(({ images, imagesOnPage }) => {
-      //       // return {
-      //       // images: [...images, ...imagesArray],
-      //       setImages(prev => [...prev, image])
-
-      //       // setImagesOnPage: imagesOnPage + imagesArray.length,
-      //       setImagesOnPage(prev => prev + imagesOnPage.length),
-      //       // };
-      //     // });
-      //   })
-      .catch(error => setError(error))
-      .finally(() => setIsLoading(state => !state));
+      // .catch(() => setError(error => error))
+      .finally(setIsLoading(isLoading => isLoading));
   }, [query, page]);
 
-  //!     need to ask about it     //
-  // getSearchRequest = query => {
-  //   this.setState({ query });
-  // };
-  const getSearchRequest = () => setQuery(query);
+  //* .        done     //
+
+  const getSearchRequest = query => {
+    setQuery(query);
+    setImages([]);
+    setPage(1);
+  };
 
   //*       done             //
   const onNextFetch = () => {
@@ -73,23 +64,19 @@ export default function App() {
   const toggleModal = () => {
     setShowModal(state => !state);
   };
-  //!     need to do it     //
+  //*     done     //
   const openModal = e => {
     const currentImageUrl = e.target.dataset.large;
     const currentImageDescription = e.target.alt;
 
     if (e.target.nodeName === 'IMG') {
       return (
-        setShowModal(!showModal) && currentImageUrl && currentImageDescription
+        setShowModal(!showModal),
+        setCurrentImageUrl(currentImageUrl),
+        setCurrentImageDescription(currentImageDescription)
       );
     }
   };
-
-  // this.setState(({ showModal }) => ({
-  //   showModal: !showModal,
-  //   currentImageUrl: currentImageUrl,
-  //   currentImageDescription: currentImageDescription,
-  // }));
 
   return (
     <div className={css.App}>
@@ -106,8 +93,8 @@ export default function App() {
       {showModal && (
         <Modal
           onClose={toggleModal}
-          // currentImageUrl={currentImageUrl}
-          // currentImageDescription={currentImageDescription}
+          currentImageUrl={currentImageUrl}
+          currentImageDescription={currentImageDescription}
         />
       )}
 
