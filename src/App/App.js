7c3c1c21235b8e056cce45de9ef8a5c9a8f1ff86
AVViktoria <56619435+AVViktoria@users.noreply.github.com>
2022-11-13 20:21:51 -0800

    Update App.js

--- src/App/App.js
+++ src/App/App.js
@@ -51,7 +51,7 @@ export default function App() {
       .catch(error => {
         // console.log(error);
         toast.error('Something went wrong!');
-        // toast.error('Change the name and try again!');
+        // throw new toast.error('Change the name and try again!');
         setIsLoading(false);
       })
       .finally(() => setIsLoading(prev => !prev));
